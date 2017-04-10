cc46ba435ff39c6aeb7e62378a9f31f06480cbcc
Vitaliy Green <point.green@gmail.com>
2017-04-10 22:03:33 +0300

    feat(webpack.config.js): remove minification from production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const ARUI_WEBPACK_TEMPLATE = require('arui-presets/webpack.base');
-const ARUI_WEBPACK_PROD_TEMPLATE = require('arui-presets/webpack.production');
 const ARUI_WEBPACK_DEV_TEMPLATE = require('arui-presets/webpack.development');
 
 const IS_PRODUCTION = (process.env.NODE_ENV === 'production');
@@ -15,16 +13,11 @@ const webpackConfig = merge.smart(
             path: path.resolve('dist'),
             filename: 'cn.js',
             libraryTarget: 'commonjs2'
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            })
-        ]
+        }
     }
 );
 
 module.exports = merge.smart(
     webpackConfig,
-    IS_PRODUCTION ? ARUI_WEBPACK_PROD_TEMPLATE : ARUI_WEBPACK_DEV_TEMPLATE
+    !IS_PRODUCTION && ARUI_WEBPACK_DEV_TEMPLATE
 );
